5c01c01c7bca508f4d7be18dd0a6d58c611dc998
ZxlDragonDoctor <2252406579@qq.com>
2025-06-27 22:20:03 +0800

    Ai应用开发基础使用

---
+++ src/main/java/com/zxl/zxlaiagent/ZxlAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.zxl.zxlaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZxlAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZxlAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zxl/zxlaiagent/ZxlAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxl.zxlaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZxlAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
